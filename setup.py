574106adf5a20ac572e3bc3faa9ce3b7cafc3673
OlawoyeTaofeek <habephe21@gmail.com>
2025-08-31 22:57:22 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Taofeek",
+    author_email="oladipupoolawoye26@gmail.com",
+    packages=find_packages()
+)
